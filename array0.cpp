49c22eb5b06dec722d070360839c776779105efa
ibnuzaman <ibnuzaman2133@gmail.com>
2022-05-14 00:55:41 +0700

    update nama repo

--- array0.cpp
+++ array0.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int main(){
     int i,total;
-    int jum_mhs[7]={40,39,41,36,38,34,42};
+    int jum_mhs[7]={40,39,41,36,38,34,50};
     total = 0;
     for(i =1 ; i <7 ; i++){
         cout<<"Jumlah Mahasiswa Kelas "<<i<< " : "<<jum_mhs[i]<<endl;
